bb6a25134ad00c0f17e0b205aeeed3057e179cd5
lackeyi <lackey_i@wustl.edu>
2024-05-23 16:48:52 -0500

    BROKEN: stubbing RC auth

--- src/features/auth-page/auth-api.ts
+++ src/features/auth-page/auth-api.ts
@@ -53,11 +53,67 @@ const configureIdentityProvider = () => {
     );
   }
 
+  if (process.env.REDCAP_API_KEY != null) {
+    providers.push(
+      CredentialsProvider({
+        name: "redcap",
+        credentials: {
+          username: { label: "Username", type: "text", placeholder: "dev" },
+          password: { label: "Password", type: "password" },
+        },
+        async authorize(credentials, req): Promise<any> {
+          console.log('using redcap....');
+          if (!credentials?.password || !credentials?.username) return false;
+
+          const baseUrl = "https://redcap.wustl.edu/redcap/api";
+            // process.env.REDCAP_API_URL ??
+            // "https://redcapdev.wustl.edu/redcap/api/";
+
+          const queryString =
+            `?token=${process.env.REDCAP_API_KEY}&content=record&action=export&format=json&type=flat&fields[0]=creds_user&fields[1]=creds_pass&fields[2]=creds_enabled` +
+            `&rawOrLabel=raw&rawOrLabelHeaders=raw&exportCheckboxLabel=false&exportSurveyFields=false&exportDataAccessGroups=false&returnFormat=json`;
+            // + `&filterLogic=[creds_user]='${credentials?.username}' AND [creds_pass] = '${credentials?.password}' AND [creds_enabled] = 1`;
+
+          const res = await fetch({
+            url: baseUrl + queryString,
+            method: "POST",
+            headers: {
+              "Content-Type": "application/x-www-form-urlencoded",
+              "Accept": "application/json",
+            },
+          });
+
+          if (res.ok) {
+
+            //TODO: check for RC error in JSON object
+            //{"creds_user":"test","creds_pass":"test","creds_enabled":"1"}
+            const data = await res.json();
+            
+            console.log('data:', data);
+            if (data.length == 1) {
+              return {
+                id: hashValue(data[0].creds_user),
+                name: data[0].creds_user,
+                email: data[0].creds_user,
+                isAdmin: false,
+                image: "",
+              };
+            }
+          }else{
+            console.log('bad response', res);
+            
+          }
+          return false;
+        },
+      })
+    );
+  }
+
   // If we're in local dev, add a basic credential provider option as well
   // (Useful when a dev doesn't have access to create app registration in their tenant)
   // This currently takes any username and makes a user with it, ignores password
   // Refer to: https://next-auth.js.org/configuration/providers/credentials
-  if (process.env.NODE_ENV === "development") {
+  if (process.env.NODE_ENV === "development2") {
     providers.push(
       CredentialsProvider({
         name: "localdev",
