f158fd0dbd5cf5b3f237dba315b6103f5010349c
lackeyi <lackey_i@wustl.edu>
2024-05-24 10:05:22 -0500

    adding name, email, and isAdmin

--- src/features/auth-page/redcap-auth.ts
+++ src/features/auth-page/redcap-auth.ts
@@ -1,6 +1,20 @@
 import CredentialsProvider from "next-auth/providers/credentials";
 import { hashValue } from "./helpers";
 import https from "https";
+import querystring from "querystring";
+
+///Add this configuration to your .env
+// Adjust field names as needed
+//
+// #REDCap Login config
+// REDCAP_API_KEY={your token}
+// REDCAP_API_URL=redcap.wustl.edu
+// REDCAP_USER_FIELD="creds_user"
+// REDCAP_PASS_FIELD="creds_pass"
+// REDCAP_ENABLED_FIELD="creds_enabled"
+// REDCAP_ADMIN_FIELD="creds_is_admin"
+// REDCAP_EMAIL_FIELD="demo_email"
+// REDCAP_NAME_FIELD="demo_name"
 
 const redcapCredentialsProvider = CredentialsProvider({
   name: "redcap",
@@ -8,17 +22,45 @@ const redcapCredentialsProvider = CredentialsProvider({
     username: { label: "Username", type: "text", placeholder: "dev" },
     password: { label: "Password", type: "password" },
   },
-  async authorize(credentials, req1): Promise<any> {
-    console.log("using redcap....");
+  async authorize(credentials): Promise<any> {
+    console.log("Signing in with REDCap....");
     if (!credentials?.password || !credentials?.username) return false;
 
     const host = process.env.REDCAP_API_URL ?? "redcapdev.wustl.edu";
+    const usernameField = process.env.REDCAP_USER_FIELD ?? "creds_user";
+    const passwordField = process.env.REDCAP_PASS_FIELD ?? "creds_pass";
+    const enabledField = process.env.REDCAP_ENABLED_FIELD ?? "creds_enabled";
+    const emailField = process.env.REDCAP_EMAIL_FIELD ?? "creds_user";
+    const nameField = process.env.REDCAP_NAME_FIELD ?? "creds_user";
+    const isAdminField = process.env.REDCAP_ADMIN_FIELD ?? "creds_is_admin";
 
-    const queryString =
-      `token=${process.env.REDCAP_API_KEY}&content=record&action=export&format=json&type=flat&fields[0]=creds_user&fields[1]=creds_pass&fields[2]=creds_enabled` +
-      `&rawOrLabel=raw&rawOrLabelHeaders=raw&exportCheckboxLabel=false&exportSurveyFields=false&exportDataAccessGroups=false&returnFormat=json` +
-      `&filterLogic=[creds_user]='${credentials?.username}' AND [creds_pass] = '${credentials?.password}' AND [creds_enabled] = 1`;
+    const formData = {
+      token: process.env.REDCAP_API_KEY,
+      content: "record",
+      action: "export",
+      format: "json",
+      type: "flat",
+      //NOTE: REDCap uses a non-standard way to handle arrays in form data.
+      // We cannot use a typical array like =>  fields: ["creds_user", "creds_enabled"],
+      // Instead we need to define the array like this:
+      "fields[0]": usernameField,
+      "fields[1]": enabledField,
+      "fields[2]": emailField,
+      "fields[3]": nameField,
+      "fields[4]": isAdminField,
+      rawOrLabel: "raw",
+      rawOrLabelHeaders: "raw",
+      exportCheckboxLabel: "false",
+      exportSurveyFields: "false",
+      exportDataAccessGroups: "false",
+      returnFormat: "json",
+      filterLogic:
+        `[${usernameField}]='${credentials?.username}'` +
+        ` AND [${passwordField}] = '${credentials?.password}'` +
+        ` AND [${enabledField}] = 1`,
+    };
 
+    let formDataString = querystring.stringify(formData);
     const options = {
       hostname: host,
       port: 443,
@@ -27,11 +69,11 @@ const redcapCredentialsProvider = CredentialsProvider({
       headers: {
         "Content-Type": "application/x-www-form-urlencoded",
         Accept: "application/json",
-        "Content-Length": Buffer.byteLength(queryString),
+        "Content-Length": Buffer.byteLength(formDataString),
       },
     };
 
-    return await new Promise((resolve) => {
+    return await new Promise((resolve, reject) => {
       const apiRequest = https.request(options, async (res) => {
         let body = "";
 
@@ -47,30 +89,41 @@ const redcapCredentialsProvider = CredentialsProvider({
           ) {
             const data = JSON.parse(body);
             if (data.length == 1) {
+              
+              let email = data[0][emailField].toString();
+              if(email == null) email = data[0][usernameField];
+              if (!email?.includes("@")) email += "@redcap";
+
+              let isAdmin = false;
+              if (data[0][isAdminField] == true) isAdmin = true;
+
               const user = {
-                id: hashValue(data[0].creds_user + "@redcap"),
-                name: data[0].creds_user,
-                email: data[0].creds_user + "@redcap",
-                isAdmin: false,
+                id: hashValue(email),
+                name: data[0][nameField],
+                email: email,
+                isAdmin,
                 image: "",
               };
 
               console.log("Logged in", user);
               resolve(user);
-              return user;
+            } else {
+              console.warn("Invalid login attempt");
+              resolve(null);
             }
           } else {
-            console.log("bad response", res.statusCode, res);
+            console.error("Bad Response", res.statusCode, res);
+            reject(new Error(`Bad Response: ${res.statusCode}`));
           }
-          return false;
         });
       });
 
       apiRequest.on("error", (error) => {
         console.error(error);
+        reject(new Error(error.message));
       });
 
-      apiRequest.write(queryString);
+      apiRequest.write(formDataString);
       apiRequest.end();
     });
   },
