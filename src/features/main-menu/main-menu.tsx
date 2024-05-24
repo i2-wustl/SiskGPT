4b6eeb0047f29f944729ee55c9fb41177aaf1965
lackeyi <lackey_i@wustl.edu>
2024-05-24 13:13:33 -0500

    remove menu options for non-admins

--- src/features/main-menu/main-menu.tsx
+++ src/features/main-menu/main-menu.tsx
@@ -32,37 +32,40 @@ export const MainMenu = async () => {
           </MenuItem>
           <MenuTrayToggle />
         </MenuItemContainer>
-        <MenuItemContainer>
-          <MenuItem tooltip="Chat">
-            <MenuLink href="/chat" ariaLabel="Go to the Chat page">
-              <MessageCircle {...menuIconProps} />
-            </MenuLink>
-          </MenuItem>
-          <MenuItem tooltip="Persona">
-            <MenuLink href="/persona" ariaLabel="Go to the Persona configuration page">
-              <VenetianMask {...menuIconProps} />
-            </MenuLink>
-          </MenuItem>
-          <MenuItem tooltip="extensions">
-            <MenuLink href="/extensions" ariaLabel="Go to the Extensions configuration page">
-              <PocketKnife {...menuIconProps} />
-            </MenuLink>
-          </MenuItem>
-          <MenuItem tooltip="prompts">
-            <MenuLink href="/prompt" ariaLabel="Go to the Prompt Library configuration page">
-              <Book {...menuIconProps} />
-            </MenuLink>
-          </MenuItem>
-          {user.isAdmin && (
-            <>
+        {user.isAdmin && (
+          <>
+            <MenuItemContainer>
+              <MenuItem tooltip="Chat">
+                <MenuLink href="/chat" ariaLabel="Go to the Chat page">
+                  <MessageCircle {...menuIconProps} />
+                </MenuLink>
+              </MenuItem>
+
+              <MenuItem tooltip="Persona">
+                <MenuLink href="/persona" ariaLabel="Go to the Persona configuration page">
+                  <VenetianMask {...menuIconProps} />
+                </MenuLink>
+              </MenuItem>
+              <MenuItem tooltip="extensions">
+                <MenuLink href="/extensions" ariaLabel="Go to the Extensions configuration page">
+                  <PocketKnife {...menuIconProps} />
+                </MenuLink>
+              </MenuItem>
+              <MenuItem tooltip="prompts">
+                <MenuLink href="/prompt" ariaLabel="Go to the Prompt Library configuration page">
+                  <Book {...menuIconProps} />
+                </MenuLink>
+              </MenuItem>
+
               <MenuItem tooltip="reporting">
                 <MenuLink href="/reporting" ariaLabel="Go to the Admin reporting" >
                   <Sheet {...menuIconProps} />
                 </MenuLink>
               </MenuItem>
-            </>
-          )}
-        </MenuItemContainer>
+
+            </MenuItemContainer>
+          </>
+        )}
         <MenuItemContainer>
           <MenuItem tooltip="Profile">
             <UserProfile />
